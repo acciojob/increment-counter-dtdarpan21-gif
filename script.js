36195737358c553a60f573c1ec7851b58c07fe30
dtdarpan21-gif <dtdarpan21@gmail.com>
2026-09-23 21:16:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ let counter = 0;
+
+        const counterElement = document.getElementById("counter");
+        const incrementBtn = document.getElementById("incrementBtn");
+
+        incrementBtn.addEventListener("click", function() {
+            alert(counter);
+            counter++;
+            counterElement.textContent = counter;
+        });
